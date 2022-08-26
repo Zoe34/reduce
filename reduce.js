65dd2c93b0a9c9cd080870c23431de2db1266993
Zofia <z.przybyl11@gmail.com>
2022-08-26 13:04:08 +0000

    Create a reduce arrow function

--- reduce.js
+++ reduce.js
@@ -10,7 +10,6 @@
 const nums = [0,1,2,3,4];
 let sums = nums.reduce((acc, curr) => acc + curr, 0);
 
-console.log(sums);
 
 
 const teamMembers = [
@@ -38,5 +37,21 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+
+
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) =>{
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+
+  return acc;
+}, {})
+
+console.log(experienceByProfession);
